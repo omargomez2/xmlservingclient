d14248e1eff9b94010f677663dddd90f89fad9ff
Omar S. Gómez <omargomez2@gmail.com>
2023-02-02 23:35:58 -0500

    Update streamlit_app.py
    :)

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,30 +6,13 @@
 #-----------
 
 import streamlit
-import pandas as pd
 import requests
+import pandas as pd
 
 url = 'https://xmlserving.fly.dev/employees'
 response  = requests.get(url, headers = {"Accept":"application/xml"})
-
-streamlit.text(response.content)
-
 df = pd.read_xml(response.content)                               
 
 streamlit.title("List of Employees")
-
-#xml_data = open(feed_url, 'r').read()  # Read file
-#root = et.XML(xml_data)  # Parse XML
-
-#data = []
-#cols = []
-#for i, child in enumerate(root):
-#    data.append([subchild.text for subchild in child])
-#    cols.append(child.tag)
-
-#df = pd.DataFrame(data).T  # Write in DF and transpose it
-#df.columns = cols  # Update column names
-
-#print(data)
 
 streamlit.dataframe(df)
