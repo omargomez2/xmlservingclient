d217c458e5bd907a50e4465f131d5375faed6464
Omar S. Gómez <omargomez2@gmail.com>
2023-02-02 20:56:18 -0500

    Create streamlit_app.py
    first ver

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+#--------------------
+# 
+# Author: OG
+# Description: Streamlit app that consumes an xml document from url: https://xmlserving.fly.dev/employees
+# url: 
+#-----------
+
+import streamlit
+import pandas
+
+streamlit.title("List of Employees")
