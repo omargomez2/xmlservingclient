d92f616811f5534d1142b7866b48e7ef743c0dfd
Omar S. Gómez <omargomez2@gmail.com>
2023-02-02 21:15:05 -0500

    t5

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,31 @@ import streamlit
 import xml.etree.ElementTree as et
 import pandas as pd
 
-
-streamlit.title("List of Employees")
+import requests
+import xmltodict
 
 feed_url = 'https://xmlserving.fly.dev/employees'
 
-xml_data = open(feed_url, 'r').read()  # Read file
-root = et.XML(xml_data)  # Parse XML
+response = requests.get(feed_url)
+data = xmltodict.parse(response.content)
 
-data = []
-cols = []
-for i, child in enumerate(root):
-    data.append([subchild.text for subchild in child])
-    cols.append(child.tag)
 
-df = pd.DataFrame(data).T  # Write in DF and transpose it
-df.columns = cols  # Update column names
+streamlit.title("List of Employees")
 
-#print(df)
+
+
+#xml_data = open(feed_url, 'r').read()  # Read file
+#root = et.XML(xml_data)  # Parse XML
+
+#data = []
+#cols = []
+#for i, child in enumerate(root):
+#    data.append([subchild.text for subchild in child])
+#    cols.append(child.tag)
+
+#df = pd.DataFrame(data).T  # Write in DF and transpose it
+#df.columns = cols  # Update column names
+
+print(data)
 
 streamlit.dataframe(df)
